8e7fcfe141c415d16b46d88063c30e88ed883452
Mark Heckler <mark.heckler@gmail.com>
2021-01-31 19:17:59 -0600

    chapter10begin

--- kotlin/aircraft-positions/build.gradle.kts
+++ kotlin/aircraft-positions/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.2"
+	id("org.springframework.boot") version "2.3.5.RELEASE"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.4.21"
 	kotlin("plugin.spring") version "1.4.21"
