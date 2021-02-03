59d58b59f34c7248465634683b9b8e9b521acf1b
Mark Heckler <mark.heckler@gmail.com>
2021-02-03 11:19:36 -0600

    Updated Aircraft domain class def

--- kotlin/aircraft-positions/src/main/kotlin/com/thehecklers/aircraftpositions/PositionController.kt
+++ kotlin/aircraft-positions/src/main/kotlin/com/thehecklers/aircraftpositions/PositionController.kt
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
-
 @Controller
 class PositionController(private val repository: AircraftRepository) {
     @GetMapping("/aircraft")
