1a55d4b91edcdfb0de0cc475fade1066c12c248f
Mark Heckler <mark.heckler@gmail.com>
2021-02-03 11:15:54 -0600

    Updated Aircraft domain class def

--- kotlin/aircraft-positions/src/main/kotlin/com/thehecklers/aircraftpositions/PositionController.kt
+++ kotlin/aircraft-positions/src/main/kotlin/com/thehecklers/aircraftpositions/PositionController.kt
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToFlux
 
-
 @Controller
 class PositionController(private val repository: AircraftRepository) {
     private val client = WebClient.create("http://localhost:7634/aircraft")
