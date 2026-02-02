24ce24664cf9f9565d723a0aeb55de057069cb20
mkheck <mark.heckler@gmail.com>
2026-02-02 10:06:00 -0600

    Chapter 5 Begin Java & Kotlin

--- kotlin/sbur-rest-demo/src/main/kotlin/com/thehecklers/sburrestdemo/SburRestDemoApplication.kt
+++ kotlin/sbur-rest-demo/src/main/kotlin/com/thehecklers/sburrestdemo/SburRestDemoApplication.kt
@@ -1,5 +1,8 @@
 package com.thehecklers.sburrestdemo
 
+import jakarta.annotation.PostConstruct
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.repository.CrudRepository
@@ -8,9 +11,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.*
 import java.util.*
-import javax.annotation.PostConstruct
-import javax.persistence.Entity
-import javax.persistence.Id
 
 @SpringBootApplication
 class SburRestDemoApplication
