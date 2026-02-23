6f6c835a164be78fbb2d25850d6c85cf23b1520f
mkheck <mark.heckler@gmail.com>
2026-02-23 17:21:41 -0600

    Updated Planefinder app for Boot 4

--- planefinder/src/main/java/com/thehecklers/planefinder/Aircraft.java
+++ planefinder/src/main/java/com/thehecklers/planefinder/Aircraft.java
@@ -2,13 +2,13 @@ package com.thehecklers.planefinder;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import java.time.Instant;
 
 @Entity
