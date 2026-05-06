f3affb47c66ad1c1397d197412df93f4627418a7
mkheck <mark.heckler@gmail.com>
2026-05-06 14:26:10 -0500

    Updated MongoDB Java app, replaced embedded Mongo (no longer supported) with local run via runMongo.sh in containerscripts repo

--- java/sbur-mongo/src/main/java/com/thehecklers/sburmongo/SburMongoApplication.java
+++ java/sbur-mongo/src/main/java/com/thehecklers/sburmongo/SburMongoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SburMongoApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(SburMongoApplication.class, args);
 	}
 
